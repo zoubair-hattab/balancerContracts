84b5fee5af361e212ae21f69ec5eacc5cc92b86d
zoubair.hattab <elhattabzoubair93@gmail.com>
2022-11-14 18:03:35 +0100

    first

---
+++ Vault/contract/ReentrancyGuard.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.0;
+
+import "./BalancerErrors.sol";
+
+
+abstract contract ReentrancyGuard {
+
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+
+    uint256 private _status;
+
+    constructor() {
+        _status = _NOT_ENTERED;
+    }
+
+    /**
+     * @dev Prevents a contract from calling itself, directly or indirectly.
+     * Calling a `nonReentrant` function from another `nonReentrant`
+     * function is not supported. It is possible to prevent this from happening
+     * by making the `nonReentrant` function external, and make it call a
+     * `private` function that does the actual work.
+     */
+    modifier nonReentrant() {
+        _enterNonReentrant();
+        _;
+        _exitNonReentrant();
+    }
+
+    function _enterNonReentrant() private {
+        // On the first call to nonReentrant, _status will be _NOT_ENTERED
+        _require(_status != _ENTERED, Errors.REENTRANCY);
+
+        // Any calls to nonReentrant after this point will fail
+        _status = _ENTERED;
+    }
+
+    function _exitNonReentrant() private {
+        // By storing the original value once again, a refund is triggered (see
+        // https://eips.ethereum.org/EIPS/eip-2200)
+        _status = _NOT_ENTERED;
+    }
+}
